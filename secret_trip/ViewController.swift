8e6c785169da3c5e5725273c7b3609d8ba5d997f
ome H <sangch.go@gmail.com>
2017-10-29 21:27:31 +0900

    Initial Commit

---
+++ secret_trip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  secret_trip
+//
+//  Created by Home on 2017. 10. 29..
+//  Copyright © 2017년 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
